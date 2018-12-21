74f286a0ceac21eec463f9c2469193c1d0c5b34e
soporus <46040421+soporus@users.noreply.github.com>
2018-12-21 00:18:06 -0600

    Update sketch_js.js

--- stimulation/sketch_js.js
+++ stimulation/sketch_js.js
@@ -66,16 +66,16 @@ function draw() {
   pop();
 
   fill(map(lightY,0,height,0,255),random(map(lightY,0,height,0,64)),random(64)+64);
-  /*
+  
   textAlign(RIGHT);
   text(lightY, txtsize*4.75, height-txtsize*3);
   textAlign(LEFT);
-  text("STIMULATION", txtsize, height-txtsize*2);
+  text('STIMULATION', txtsize, height-txtsize*2);
   if(lightY > height/2) {
     fill(map(lightY,0,height,128,255),(map(lightY,0,height,64,128)),random(64)+64);
-    text("CAUTION", txtsize, height-txtsize);
+    text('CAUTION', txtsize, height-txtsize);
   }
-  */
+  
 }
 function moveLight() {
   if(lightXbit == false) {
