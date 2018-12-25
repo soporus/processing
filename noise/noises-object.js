d5a600fc1c7efc10ddb233aa2652c3072875da18
soporus <adjohnny@gmail.com>
2018-12-25 00:11:43 -0600

    added channels to object, to fascillate mixer.  added compressor

--- noise/noises-object.js
+++ noise/noises-object.js
@@ -3,11 +3,21 @@
 function SynthObject(tempsynthType,tempOsc) {
   //envelope
   //todo: remove passed env, replace with preset based on flag
+  this.channel = new p5.Gain();
+  this.LFO1Bit = true;
+  this.LFO1 = width/2;// modulator
+  this.LFO1Speed = 0;
+  this.LFO2Bit = true;
+  this.LFO2 = width/2;// modulator
+  this.LFO2Speed = 0;
   this.synthType = tempsynthType;
   this.oscType = tempOsc;
   //noise has no pitch options
   if (this.oscType === 'noise') {
     this.osc = new p5.Noise();
+  } else if (this.oscType === 'pulse'){
+    this.osc = new p5.Pulse();
+    this.osc.width(0.5);
   } else {
     this.osc = new p5.Oscillator(this.oscType);
   }
@@ -49,11 +59,30 @@ function SynthObject(tempsynthType,tempOsc) {
     this.filter.res(33);
     this.filter.freq(10000);
   } else this.filter.res(0);
-
+  this.filter.disconnect();
+  this.channel.setInput(this.filter);
   this.osc.start();
   this.osc.amp(this.Aenv);
   this.trigger = function() {
     this.Aenv.play();
     this.Penv.play();
+  }
+  this.LFO1Mod = function() {
+    if(this.LFO1bit == true) {
+      if(this.LFO1 > height) this.LFO1bit= !this.LFO1bit;
+      if(this.LFO1 <= height) this.LFO1+=this.LFO1Speed;
+    } else {
+      if(this.LFO1 <= 0) this.LFO1bit= !this.LFO1bit;
+      if(this.LFO1 > 0) this.LFO1-=this.LFO1Speed;
+    }
+  }
+  this.LFO2Mod = function() {
+    if(this.LFO2bit == true) {
+      if(this.LFO2 > height) this.LFO2bit= !this.LFO2bit;
+      if(this.LFO2 <= height) this.LFO2+=this.LFO2Speed;
+    } else {
+      if(this.LFO2 <= 0) this.LFO2bit= !this.LFO2bit;
+      if(this.LFO2 > 0) this.LFO2-=this.LFO2Speed;
+    }
   }
 }
--- noise/noises.js
+++ noise/noises.js
@@ -1,4 +1,4 @@
-var fft, analyzer, drum, drum2, drum3, synth, synth2, synth3, reverb, delay;
+var fft, analyzer, drum, drum2, drum3, synth, synth2, synth3, reverb, delay, compressor, mainChannel;
 
 var spectrum;
 // var stepTaken = false;
@@ -6,8 +6,10 @@ var scaleArray = [49, 52, 56, 58, 64, 68];
 var freqValue;
 
 function setup() {
+  mainChannel = new p5.Gain();
   reverb = new p5.Reverb();
   delay = new p5.Delay();
+  compressor = new p5.Compressor();
   delay.setType(1);
   //kick
   drum = new SynthObject('drum', 'square');
@@ -26,43 +28,63 @@ function setup() {
   //noise
   drum3 = new SynthObject('drum', 'noise');
   //synth bass
-  synth = new SynthObject('synth', 'square');
-  synth.osc.freq(0);
+  synth = new SynthObject('synth', 'pulse');
+  synth.osc.freq(220);
+  synth.LFO1Speed = 20;
   synth.filter.freq(300);
   synth.filter.res(0.5);
   synth.filter.amp(0.5);
   //synth mid
   synth2 = new SynthObject('synth', 'sawtooth');
+  synth2.LFO1Speed = 10;
   synth2.osc.freq(0);
   synth2.filter.freq(750);
   synth2.filter.res(1);
-  synth2.filter.amp(0.4);
+  synth2.filter.amp(0.5);
   synth2.osc.pan(-0.4);
-  synth2.filter.disconnect();
   //synth high
   synth3 = new SynthObject('synth', 'sawtooth');
+  synth3.LFO1Speed = 7;
   synth3.osc.freq(0);
   synth3.filter.freq(565);
   synth3.filter.res(2);
-  synth3.filter.amp(0.3);
+  synth3.filter.amp(0.4);
   synth3.osc.pan(0.4);
-  synth3.filter.disconnect();
 
   reverb.process(synth2.filter, 10, 1);
   reverb.process(synth3.filter, 10, 1);
   reverb.process(drum3.filter, 2, 8);
+  reverb.disconnect();
   // delay.process(drum2.filter, 0.025, 0.5, 900);
   delay.process(synth2.filter, 0.2, 0.8, 1900);
   delay.process(synth3.filter, 0.2, 0.8, 1900);
+  delay.disconnect();
+
+  drum.channel.connect(mainChannel);
+  drum2.channel.connect(mainChannel);
+  drum3.channel.connect(mainChannel);
+  synth.channel.connect(mainChannel);
+  synth2.channel.connect(mainChannel);
+  synth3.channel.connect(mainChannel);
+  delay.connect(mainChannel);
+  reverb.connect(mainChannel);
+  //compressor process(src, [attack], [knee], [ratio], [threshold], [release])
+  compressor.process(mainChannel, 0.003, 35, 4, -48, 0.25);
 
   createCanvas(windowWidth, windowHeight, WEBGL);
   frameRate(30);
   stroke(255, 0, 128);
-  fill(0, 64, 64);
+  noFill();
   fft = new p5.FFT(0.7, 16);
 }
 
 function draw() {
+  synth.LFO1Mod();
+  synth2.LFO1Mod();
+  synth3.LFO1Mod();
+  synth.osc.width(map(synth.LFO1, 0, height, 0.33, 0.66));
+  synth2.filter.freq(map(synth2.LFO1, 0, height, 500, 1000));
+  synth3.filter.freq(map(synth3.LFO1, 0, height, 700, 1200));
   background(0);
   //kick
   if ((frameCount % 32) === 0 || (frameCount % 72) === 0) {
