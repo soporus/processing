6355426c89bf93fadf2f0703bf384aa03d56699c
soporus <adjohnny@gmail.com>
2019-01-06 01:14:56 -0600

    added different rotation modes

--- bcube/bcube.js
+++ bcube/bcube.js
@@ -1,32 +1,24 @@
 let angle = 0;
 const barWidth = 80;
 const barPadding = 8;
-let ma;
 let maxD;
 const w = barWidth / 2;
 let toggleSpin = false;
+let spinType = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  ma = atan(cos(QUARTER_PI));
   maxD = dist(0, 0, 300, 300);
   noFill();
   strokeWeight(4);
-  stroke(255, 0, 255);
-  // normalMaterial();
 }
 
 function draw() {
-  // directionalLight(255, 0, 192, -1, 0, -1);
-  // directionalLight(0, 192, 255, 1, 0, -1);
-  // ortho(-windowWidth, windowHeight, windowWidth, -windowHeight, 0, 1000);
   rotateX(PI / 3);
   rotateZ(angle / 10);
-  // rotateY(-QUARTER_PI);
   background(0);
-  // fill(255);
   let offset = 0;
-  for (let z = 0; z < height; z += w + barPadding) {
+  for (let z = 0; z < height; z += w + barWidth) {
     for (let x = 0; x < width; x += w) {
       let d = dist(x, 0, width / 2, 0);
       let offset = map(d, -maxD, maxD, -PI, PI);
@@ -36,9 +28,21 @@ function draw() {
       stroke(c, map(c, 0, 255, 192, 0), c / 1.5 + (z / 2));
       push();
       if (toggleSpin === true) {
-        rotateY(PI / map(c, 0, 255, -45, 45));
+        switch (true) {
+          case spinType === 0:
+            rotateX(PI / map(c, 0, 255, -45, 45));
+            break;
+          case spinType === 1:
+            rotateY(PI / map(c, 0, 255, -45, 45));
+            break;
+          case spinType === 2:
+            rotateZ(PI / map(c, 0, 255, -45, 45));
+            break;
+          default:
+            break;
+        }
       }
-      box((x - (width / 2)) + (w / 2), h - offset * 20, x - (width / 2) + (w / 2) - w);
+      box((x - (width / 2)), h - offset * 20, x - (width / 2));
       pop();
       offset += a;
     }
@@ -52,5 +56,18 @@ function windowResized() {
 
 function mousePressed() {
   toggleSpin = !toggleSpin;
+  switch (true) {
+    case spinType === 0:
+      spinType = 1;
+      break;
+    case spinType === 1:
+      spinType = 2;
+      break;
+    case spinType === 2:
+      spinType = 0;
+      break;
+    default:
+      break;
+  }
   return false;
 }
